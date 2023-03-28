48c8e4cdb861e5e8fb9d0c204ff752996326e066
dannygallo11 <dannygallo11@gmail.com>
2023-03-28 16:48:35 -0500

    react-bootstrap download and test
    Downloaded Bootstrap and react-bootstrap and tested the button and progress bar by using the components from react-bootstrap and the css from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My React App</h1>
+      <Button>This is button</Button>
+      <ProgressBar now={20}/>
     </div>
   );
 }
