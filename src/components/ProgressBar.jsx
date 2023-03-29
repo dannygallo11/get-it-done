acd99fd8de668b19ed50cc0100a02c053b523be6
dannygallo11 <dannygallo11@gmail.com>
2023-03-28 22:05:38 -0500

    Progress bar customization
    created CSS file for progress bar to customize color

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import "bootstrap/dist/css/bootstrap.min.css";
+import '../css/ProgressBar.css';
 
 function TaskCompletionBar() {
-  return (
-    <div>
-        <ProgressBar now={20}/>
-    </div>
-  )
+    const percentage = 70;
+    const barColor = percentage < 50 ? "orange" : percentage > 49 || percentage < 100 ? "yellow" : "green";
+
+    return <ProgressBar now={percentage} label={`${percentage}%`}/>
 };
 
 export default TaskCompletionBar;
+
+
+// style={{backgroundColor: barColor}}
