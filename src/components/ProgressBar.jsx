26eeebc9a3673920ef9064af470711372d402959
dannygallo11 <dannygallo11@gmail.com>
2023-03-29 08:39:50 -0500

    Progress Bar CSS

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import "bootstrap/dist/css/bootstrap.min.css";
-import '../css/ProgressBar.css';
+// import '../css/ProgressBar.css';
 
 function TaskCompletionBar() {
     const percentage = 70;
