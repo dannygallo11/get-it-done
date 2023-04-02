88b245153f6a1204a7fbe2e7c09094937ac5c363
dannygallo11 <dannygallo11@gmail.com>
2023-04-01 21:10:44 -0500

    Started to establish DB connection

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import ActionCard from './css/ActionCard';
 
+// used for backend to connect to mongoDB
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+
+
 // import Button from 'react-bootstrap/Button';
 // import ActionItem from './components/ActionItem';
 // import TaskCompletionBar from './components/ProgressBar';
 
+// Connection point for DB
+mongoose.connect("mongodb://locoalhost:27017/getitdoneDB");
+
+//Creating DB Schema
+const actionItems = {
+  key:Number,
+  Date:Date,
+  Ckbx_value:Boolean,
+  item:String,
+  prio_value:Boolean
+}
 
 function App() {
   return (
