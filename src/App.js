c49a2274e8f620f2ec637239f9a5c272dd6fd65b
dannygallo11 <dannygallo11@gmail.com>
2023-04-03 11:59:02 -0500

    added mongoose setup code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import ActionCard from './css/ActionCard';
+// import Button from 'react-bootstrap/Button';
+// import ActionItem from './components/ActionItem';
+// import TaskCompletionBar from './components/ProgressBar';
 
 // used for backend to connect to mongoDB
 const express = require("express");
@@ -8,23 +11,49 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 
-
-// import Button from 'react-bootstrap/Button';
-// import ActionItem from './components/ActionItem';
-// import TaskCompletionBar from './components/ProgressBar';
-
 // Connection point for DB
-mongoose.connect("mongodb://locoalhost:27017/getitdoneDB");
+mongoose.connect("mongodb://locoalhost:27017/GetItDoneDB");
 
-//Creating DB Schema
-const actionItems = {
-  key:Number,
-  Date:Date,
-  Ckbx_value:Boolean,
-  item:String,
-  prio_value:Boolean
-}
+//Input data
+// 1. Creating DB Schema
+const actionItemsSchema = new mongoose.Schema({
+  _id: Number,
+  cr_date: Date,
+  due_date: Date,
+  status: String,
+  item: String,
+  action_type: String,
+  ckbx_value: Boolean,
+  prio_value: Boolean,
+  deadline_task: String,
+  deadline_date: Date
+});
 
+// 2. Create Mongoose Model. This will create a collection (it will convert collection name to plural)
+const ActionItem = mongoose.model("actionitem", actionItemsSchema);
+
+// 3. Create data
+// create new document with existing model
+const actionItem = new ActionItem ({
+  _id: 1,
+  cr_date: new Date(),
+  due_date: new Date(),
+  status: "not started",
+  item: "code MongoDB connection",
+  action_type: "Coding",
+  ckbx_value: false,
+  prio_value: true,
+  deadline_task: "Connect to DB",
+  deadline_date: 2023-05-30
+});
+
+// 4. Save document
+actionItem.save();
+
+
+
+
+// App component
 function App() {
   return (
     <div>
@@ -34,3 +63,4 @@ function App() {
 }
 
 export default App;
+
